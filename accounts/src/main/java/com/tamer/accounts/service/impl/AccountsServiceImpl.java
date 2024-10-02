8067832a2737b2c88a6f01af9560d2c7904aefe3
mohamedtamer0 <mt4452242@gmail.com>
2024-10-02 17:46:58 +0300

    update

---
+++ accounts/src/main/java/com/tamer/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,67 @@
+package com.tamer.accounts.service.impl;
+
+import com.tamer.accounts.constants.AccountsConstants;
+import com.tamer.accounts.dto.CustomerDto;
+import com.tamer.accounts.entity.Accounts;
+import com.tamer.accounts.entity.Customer;
+import com.tamer.accounts.exception.CustomerAlreadyExistsException;
+import com.tamer.accounts.mapper.CustomerMapper;
+import com.tamer.accounts.repository.AccountsRepository;
+import com.tamer.accounts.repository.CustomerRepository;
+import com.tamer.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateAccount(CustomerDto customerDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        return false;
+    }
+}
