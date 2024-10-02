7fb0ec3a30aff5dab1d8dedcaaa01196ca45294a
mohamedtamer0 <mt4452242@gmail.com>
2024-10-02 23:53:55 +0300

    update

---
+++ cards/src/test/java/com/tamer/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
